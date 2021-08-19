cfea10adea5073b59e6c8b6b11d8b7a34e5cd0c7
Eduardo Lima <edulima@gmail.com>
2021-08-19 18:47:36 -0300

    Criando divisão

--- src/test/java/com/edulima01/pipelines/demo/DemoApplicationTests.java
+++ src/test/java/com/edulima01/pipelines/demo/DemoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.edulima01.pipelines.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void thatDivisionsByZeroThrowBadRequestException() throws Exception {
+		this.mockMvc.perform(get("/Math/Divide?first=4&second=0")).andExpect(status().isBadRequest());
 	}
 
 }
